9010a4913e1bdc9381359c14cdfd57e19997068f
Hemanshu Waghulde <hims.waghulde@gmail.com>
2020-09-28 13:21:34 +0530

    Not found page implemented

--- client/pages/homeLayoutPage.tsx
+++ client/pages/homeLayoutPage.tsx
@@ -3,6 +3,7 @@ import LandingPage from './landingPage'
 import SearchPage from './searchPage'
 import { Route, Switch } from 'react-router-dom'
 import DetailsPage from "./detailsPage"
+import NotFoundPage from "./notFoundPage"
 
 const homeLayoutPage = (): JSX.Element => {
     return (
@@ -10,6 +11,7 @@ const homeLayoutPage = (): JSX.Element => {
             <Route path="/index" component={ LandingPage } exact />
             <Route path="/search" component={ SearchPage } />
             <Route path="/details/:id" component={ DetailsPage } />
+            <Route component={ NotFoundPage } />
         </Switch>
     )}
 
---
+++ client/pages/notFoundPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div className="row justify-content-md-center justify-content-s-center">
+            <div className="col-sm-8 col-md-5">
+                <br />
+                <h2>Requested page not found</h2>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundPage
